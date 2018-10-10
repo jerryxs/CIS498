8fa6c098621f37fc13fc841745554af5b44da9a1
istpierre <40343344+istpierre@users.noreply.github.com>
2018-10-10 15:55:43 -0400

    Added Multiple Text boxes
    Multiple text boxes to ask for license data. does not save anywhere yet

--- EManage/App.js
+++ EManage/App.js
@@ -2,25 +2,64 @@ import React from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = { text: 'Placeholder' };
+	constructor(){
+		super();
+		this.state = { 
+			fName: 'First Name:',
+			lName: 'Last Name:',
+			street: 'Address:',
+			sttwn: 'Town/State:',
+			gndr: 'Sex:'
+		};
 	}
   render() {
     return (
       <View style={styles.container}>
-		<TextInput
-			 style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({text})}
-        value={this.state.text}
+	  // First Name
+	  <Text style = {{textAlign: 'center'}}>
+		TEST
+	  </Text>
+		<TextInput 
+			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+			 placeHolder = "Enter Text"
+			 value = { this.state.fName }
 		
 		/>
-			
+		// LastName
+		<TextInput 
+			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+			 placeHolder = "Enter Text"
+			 value = { this.state.lName }
+		
+		/>
+		// Address: House Number, Street
+		<TextInput 
+			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+			 placeHolder = "Enter Text"
+			 value = { this.state.street }
+		
+		/>
+		// Town, State
+		<TextInput 
+			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+			 placeHolder = "Enter Text"
+			 value = { this.state.sttwn }
+		
+		/>
+		// Gender
+		<TextInput 
+			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+			 placeHolder = "Enter Text"
+			 value = { this.state.gndr }
+		
+		/>
 		
       </View>
     );
   }
 }
+// We used create-react-native app so we dont need this but Im going to leave it for now just in case XD
+//AppRegistry.registerComponent( 'App', () => App);
 
 const styles = StyleSheet.create({
   container: {
@@ -30,3 +69,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
