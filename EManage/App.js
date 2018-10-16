7bf2d87a8eec5d83c204a2f6ebe8b43df04206ae
NicholasCorcoran <39245904+NicholasCorcoran@users.noreply.github.com>
2018-10-16 19:13:51 -0400

    JSON
    i have no clue if this works

--- EManage/App.js
+++ EManage/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+	var fs = require('fs')
 	constructor(){
 		super();
-		this.state = { 
+		this.state = {
+			licNum: 'License Number',
 			fName: 'First Name:',
 			lName: 'Last Name:',
 			street: 'Address:',
@@ -12,48 +14,104 @@ export default class App extends React.Component {
 			gndr: 'Sex:'
 		};
 	}
+	onChangeLicNum(value){
+			this.setState({
+				licNum:value
+			});
+	}
+
+	onChangeFName(value){
+			this.setState({
+				fName:value
+			});
+	}
+	onChangeLName(value){
+			this.setState({
+				lName:value
+			});
+	}
+
+	onChangeStreet(value){
+			this.setState({
+				street:value
+			});
+	}
+	onChangeStTwn(value){
+			this.setState({
+				sttwn:value
+			});
+	}
+	onChangeGndr(value){
+			this.setState({
+				gndr:value
+			});
+	}
+	onPressEnterData(){
+		var data = JSON.stringify(state, null, 2)
+		fs.writeFile('Data.json', data)
+	}
+
   render() {
     return (
       <View style={styles.container}>
-	  // First Name
+
 	  <Text style = {{textAlign: 'center'}}>
 		TEST
 	  </Text>
-		<TextInput 
+		//license number
+		<TextInput
+			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+			 placeHolder = "Enter Text"
+			 value = { this.state.licNum }
+			 onChangeText={(value) => this.onChangeLicNum(value)}
+
+		/>
+		// First Name
+		<TextInput
 			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
 			 placeHolder = "Enter Text"
 			 value = { this.state.fName }
-		
+			 onChangeText={(value) => this.onChangeFName(value)}
+
 		/>
 		// LastName
-		<TextInput 
+		<TextInput
 			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
 			 placeHolder = "Enter Text"
 			 value = { this.state.lName }
-		
+			 onChangeText={(value) => this.onChangeLName(value)}
+
 		/>
 		// Address: House Number, Street
-		<TextInput 
+		<TextInput
 			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
 			 placeHolder = "Enter Text"
 			 value = { this.state.street }
-		
+			 onChangeText={(value) => this.onChangeStreet(value)}
+
 		/>
 		// Town, State
-		<TextInput 
+		<TextInput
 			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
 			 placeHolder = "Enter Text"
 			 value = { this.state.sttwn }
-		
+			 onChangeText={(value) => this.onChangeStTwn(value)}
+
 		/>
 		// Gender
-		<TextInput 
+		<TextInput
 			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
 			 placeHolder = "Enter Text"
 			 value = { this.state.gndr }
-		
+			 onChangeText={(value) => this.onChangeGndr(value)}
+
 		/>
-		
+		<Button
+			onPress={onPressEnterData}
+			title="Submit"
+			color='gray'
+		/>
+
       </View>
     );
   }
@@ -69,4 +127,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
