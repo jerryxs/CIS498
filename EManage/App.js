1a12b6dcacdd13312988fdfea38286c05dfb8904
NicholasCorcoran <39245904+NicholasCorcoran@users.noreply.github.com>
2018-10-29 23:09:56 -0400

    Textboxes work
    Comment out button

--- EManage/App.js
+++ EManage/App.js
@@ -1,72 +1,157 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class App extends React.Component {
-	constructor(){
-		super();
-		this.state = { 
-			fName: 'First Name:',
-			lName: 'Last Name:',
-			street: 'Address:',
-			sttwn: 'Town/State:',
-			gndr: 'Sex:'
-		};
-	}
-  render() {
-    return (
-      <View style={styles.container}>
-	  // First Name
-	  <Text style = {{textAlign: 'center'}}>
-		TEST
-	  </Text>
-		<TextInput 
-			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-			 placeHolder = "Enter Text"
-			 value = { this.state.fName }
-		
-		/>
-		// LastName
-		<TextInput 
-			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-			 placeHolder = "Enter Text"
-			 value = { this.state.lName }
-		
-		/>
-		// Address: House Number, Street
-		<TextInput 
-			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-			 placeHolder = "Enter Text"
-			 value = { this.state.street }
-		
-		/>
-		// Town, State
-		<TextInput 
-			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-			 placeHolder = "Enter Text"
-			 value = { this.state.sttwn }
-		
-		/>
-		// Gender
-		<TextInput 
-			 style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-			 placeHolder = "Enter Text"
-			 value = { this.state.gndr }
-		
-		/>
-		
-      </View>
-    );
-  }
+import React, {Component} from 'react';
+import {AppRegistry, Button, TextInput, Platform, StyleSheet, Text, View} from 'react-native';
+
+
+
+const instructions = Platform.select({
+  /*ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',*/
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    constructor(props) {
+        super(props);
+        this.state = { licNum: "",
+        dob: "",
+        fName: "",
+        lName: "",
+        address: "",
+        town: "",
+        st8: "",
+        gndr: ""
+        };
+    }
+
+
+
+    render() {
+        return (
+                <View style={{flex: 1}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+                <Text style={styles.welcome}>EManage</Text>
+                <Text style={styles.instructions}>I cant get anything else to work</Text>
+
+
+
+
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2 , textAlign: 'center' }}
+                onChangeText={(licNum) => this.setState({licNum})}
+                value={this.state.licNum}
+                placeholder = "Enter License Number"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(dob) => this.setState({dob})}
+                value={this.state.dob}
+                placeholder = "Enter DOB"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(fName) => this.setState({fName})}
+                value={this.state.fName}
+                placeholder = "First Name"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(lName) => this.setState({lName})}
+                value={this.state.lName}
+                placeholder = "Last Name"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(address) => this.setState({address})}
+                value={this.state.address}
+                placeholder = "Address"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(town) => this.setState({town})}
+                value={this.state.town}
+                placeholder = "Town"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(st8) => this.setState({st8})}
+                value={this.state.st8}
+                placeholder = "State"
+                />
+
+                <TextInput
+                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                onChangeText={(gndr) => this.setState({gndr})}
+                value={this.state.gndr}
+                placeholder = "Gender"
+                />
+
+                <Button
+                onPress={onPressLearnMore}
+                title="Learn More"
+                />
+
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+
+                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+
+
+
+                </View>
+                );
+    }
 }
-// We used create-react-native app so we dont need this but Im going to leave it for now just in case XD
-//AppRegistry.registerComponent( 'App', () => App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  license: {
+    height: 40,
+    width: 100,
+    borderColor: 'gray',
+    borderWidth: 2
+ },
+
 });
 
+//needed since we are not using create-react-native-app
+AppRegistry.registerComponent('Test', () => UselessTextInput);
