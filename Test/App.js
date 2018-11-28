44209569dbd9ee0df6c9a6630e2ab2470541a37e
Ian <vladfan12@gmail.com>
2018-11-28 18:43:09 -0500

    Sending Messages Peer to Peer
    Able to sucessfully locate nodes and send messages between them

--- Test/App.js
+++ Test/App.js
@@ -1,314 +1,172 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-	AppRegistry,
-	Button,
-	TextInput,
-	Platform,
-	StyleSheet,
-	Text,
-	ScrollView,
-	View
-	} from 'react-native';
+  AppRegistry,
+  StyleSheet,
+  View,
+  Button
+} from 'react-native';
 import {
-	createStackNavigator,
-	stackActions,
-	NavigationActions,
-} from 'react-navigation';
-import nodejs from 'nodejs-mobile-react-native';
-import RNFetchBlob from 'rn-fetch-blob';
-import { sha256 } from 'react-native-sha256';
-
-
-const instructions = Platform.select({
-
-  /*ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-	android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',*/
-
-});
-
-const dirs = RNFetchBlob.fs.dirs
-var path = dirs.DocumentDir + '/my.csv';
-	/*
-	 * Check to see if the CSV file exists
-	 * If it does then skip this portion as to not add the headers again
-	 * If not then create this file at the file path for android and add headers
-	 */
-	 RNFetchBlob.fs.exists(path)
-.then((exist) => {
-    if(exist === false){
-			RNFetchBlob.fs.writeStream(path, 'base64', true)
-		     .then((stream) => {
-		         stream.write(RNFetchBlob.base64.encode('licNum, DOB, fName, lName, address, town, state, gender ' + '\n'))
-		         return stream.close()
-		     })
-		}
-})
+  initialize,
+  isSuccessfulInitialize,
+  startDiscoveringPeers,
+  stopDiscoveringPeers,
+  unsubscribeFromPeersUpdates,
+  unsubscribeFromConnectionInfoUpdates,
+  subscribeOnConnectionInfoUpdates,
+  subscribeOnPeersUpdates,
+  connect,
+  disconnect,
+  createGroup,
+  removeGroup,
+  getAvailablePeers,
+  sendFile,
+  receiveFile,
+  getConnectionInfo,
+  receiveMessage,
+  sendMessage
+} from 'react-native-wifi-p2p';
+import { PermissionsAndroid } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+	constructor(props){
+	super(props);
+  this.state = {
+    devices: []
+  };
+}
 
-	//Add states for input boxes
-    constructor(props) {
-        super(props);
-        this.state = {
-			licNum: "",
-			dob: "",
-			fName: "",
-			lName: "",
-			address: "",
-			town: "",
-			st8: "",
-			gndr: ""
-        };
-    }
+  componentDidMount() {
+    initialize();
+    isSuccessfulInitialize()
+        .then(status => console.warn(status));
+    startDiscoveringPeers()
+        .then(() => console.warn('Sucessfull'))
+        .catch(err => console.warn(err));
 
-		onPressTest(){
-			var num = 1;
-			var temp = 10000000;
-			var csvData = [];
-			for(num; num < 5000; num++){
+    subscribeOnPeersUpdates(({ devices }) => this.handleNewPeers(devices));
+    subscribeOnConnectionInfoUpdates(this.handleNewInfo);
+  }
 
-				csvData = [
-				10000000,
-				"12/4/95",
-				"Nick",
-				"Corcoran",
-				"123 test st",
-				"testville",
-				"Testachusetts",
-				"t"
+  componentWillUnmount() {
+    unsubscribeFromConnectionInfoUpdates((event) => console.log('unsubscribeFromConnectionInfoUpdates', event));
+    unsubscribeFromPeersUpdates((event) => console.log('unsubscribeFromPeersUpdates', event));
+  }
 
-			 ];
-			 csvData[0] = 10000000;
-			 csvData[0] = csvData[0] + num;
-			 csvData[0] = "S" + csvData[0];
+  handleNewInfo = (info, sceondParam) => {
+    console.warn(64646776467, info);
+  };
 
-			 sha256(csvData[0]).then( hash => {
-			 csvData[0] = hash
-			 RNFetchBlob.fs.appendFile(path, RNFetchBlob.base64.encode(csvData + '\n'), 'base64')
-						 .then(()=>{ return;})
-			 })
+  handleNewPeers = (peers) => {
+    console.log(754862162442324, peers);
+    this.setState({ devices: peers });
+  };
 
-				csvData[0] = 10000000;
-			}
-			console.warn("done");
-		}
-		getDate()
-		{
-			var today = new Date();
-    	var birthDate = new Date(this.state.dob);
-	    var age = today.getFullYear() - birthDate.getFullYear();
-	    var m = today.getMonth() - birthDate.getMonth();
-	    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
-			{
-	        age--;
-	    }
-			console.warn(age);
-		}
+  connectToFirstDevice = () => {
+      console.warn(this.state.devices[0]);
+      connect(this.state.devices[0].deviceAddress)
+          .then(() => console.warn('Successfully connected'))
+          .catch(err => console.error('Something gone wrong. Details: ', err));
+  };
 
+  disconnectFromDevice = () => {
+      disconnect()
+          .then(() => console.log(2423435423, 'Successfully disconnected'))
+          .catch(err => console.error(2423435423, 'Something gone wrong. Details: ', err));
+  };
 
+  onCreateGroup = () => {
+      createGroup()
+          .then(() => console.warn('Group created successfully!'))
+          .catch(err => console.error('Something gone wrong. Details: ', err));
+  };
 
-	//Function for submit button
-    onPressEnterData(){
-	  //When submit is pressed, an array is populated with the new state of each input box
-	  var csvData = [
+  onRemoveGroup = () => {
+      removeGroup()
+          .then(() => console.warn('Currently you don\'t belong to group!'))
+          .catch(err => console.error('Something gone wrong. Details: ', err));
+  };
 
-		 this.state.licNum,
-		 this.state.dob,
-		 this.state.fName,
-		 this.state.lName,
-		 this.state.address,
-		 this.state.town,
-		 this.state.st8,
-		 this.state.gndr
+  onStopInvestigation = () => {
+      stopDiscoveringPeers()
+          .then(() => console.warn('Stopping of discovering was successful'))
+          .catch(err => console.error(`Something is gone wrong. Maybe your WiFi is disabled? Error details`, err));
+  };
 
-	  ];
+  onStartInvestigate = () => {
+      startDiscoveringPeers()
+          .then(status => console.warn(33333333, `Status of discovering peers: ${status}`))
+          .catch(err => console.error(`Something is gone wrong. Maybe your WiFi is disabled? Error details: ${err}`));
+  };
 
-		sha256(csvData[0]).then( hash => {
-			csvData[0] = hash
-    //console.warn(csvData[0]);
-		})
-		sha256(csvData[4]).then( hash => {
-			csvData[4] = hash
-    //console.warn(csvData[4]);
-		})
+  onGetAvailableDevices = () => {
+      getAvailablePeers()
+          .then(peers => console.warn(peers));
+  };
 
-	  //console.warn("Directory: ", dirs.DocumentDir);
-	  //Array is indexed properly can store csvData[0] into string to dup check
-	  //console.warn("Array[0]: ", csvData[0]);
-		  //Read file before adding new info to it
-		  //Currently just reads the file and spits out the info in a warn
-		  RNFetchBlob.fs.readStream(path, 'utf8')
-			.then((stream) => {
-				let data = ''
-				stream.open()
-				stream.onData((chunk) => {
-					data += chunk
+  onSendMessage = () => {
+      sendMessage("Hello world!")
+        .then(() => console.warn('Message sent successfully'))
+        .catch(err => console.log('Error while message sending', err));
+  };
 
-				})
+  onReceiveMessage = () => {
+      receiveMessage()
+          .then((msg) => alert('Message received successfully ' +  msg))
+          .catch(err => console.warn('Error while message receiving', err))
+  };
 
-				stream.onEnd(() => {
-					if(data.includes(csvData[0])){
-						alert('Duplicate Warning!' + '\n' + 'Guest Has Already Entered The Event')
-						//console.warn(csvData[0], " Is already in the file");
-					}
-					else{
+  onGetConnectionInfo = () => {
+    getConnectionInfo()
+        .then(info => console.warn(info));
+  };
 
-						//Append the input data to the file
-						RNFetchBlob.fs.writeStream(path, 'base64', true)
-						.then((stream) => {
-							stream.write(RNFetchBlob.base64.encode(csvData + '\n'))
-							return stream.close()
-						})
-						alert('Guest Added To The Event List!')
-					}
-
-				})
-			})
-		/*	this.setState({licNum: "",
-										dob: "",
-										fName: "",
-										lName: "",
-										address: "",
-										town: "",
-										st8: "",
-										gndr: ""
-							        })*/
-
-	  // just makes a warning pop up with the data entered in the text boxes
-	  //console.warn(csvData);
-
-    }
-	//Backend for zyre
-	/*componentWillMount()
-  {
-    nodejs.start('main.js');
-    nodejs.channel.addListener(
-      'message',
-      (msg) => {
-        alert('From node: ' + msg);
-      },
-      this
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Connect"
+          onPress={this.connectToFirstDevice}
+        />
+        <Button
+          title="Disconnect"
+          onPress={this.disconnectFromDevice}
+        />
+        <Button
+          title="Create group"
+          onPress={this.onCreateGroup}
+        />
+        <Button
+          title="Remove group"
+          onPress={this.onRemoveGroup}
+        />
+        <Button
+          title="Investigate"
+          onPress={this.onStartInvestigate}
+        />
+        <Button
+          title="Prevent Investigation"
+          onPress={this.onStopInvestigation}
+        />
+        <Button
+          title="Get Available Devices"
+          onPress={this.onGetAvailableDevices}
+        />
+        <Button
+          title="Get connection Info"
+          onPress={this.onGetConnectionInfo}
+        />
+        <Button
+          title="Send message"
+          onPress={this.onSendMessage}
+        />
+        <Button
+          title="Receive message"
+          onPress={this.onReceiveMessage}
+        />
+      </View>
     );
-  }*/
-
-
-
-    render() {
-        return (
-								<ScrollView style={{flex:1}}>
-                <View style={{flex: 1, justifyContent: 'center', margin: 10, alignItems: 'center'}}>
-                <View style={{flex: 1, backgroundColor: 'powderblue', margin: 10, justifyContent: 'center', alignItems: 'center'}} />
-								<View style={{flex: 1, backgroundColor: 'skyblue'}} />
-
-                <View style={{flex: 1, backgroundColor: 'steelblue'}} />
-                <Text style={styles.welcome}>EManage</Text>
-                <Text style={styles.instructions}>Please Enter the Following Data</Text>
-
-
-
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2 , textAlign: 'center' }}
-                onChangeText={(licNum) => this.setState({licNum})}
-                value={this.state.licNum}
-                placeholder = "License Number"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(dob) => this.setState({dob})}
-                value={this.state.dob}
-                placeholder = "MM/DD/YY"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(fName) => this.setState({fName})}
-                value={this.state.fName}
-                placeholder = "First Name"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(lName) => this.setState({lName})}
-                value={this.state.lName}
-                placeholder = "Last Name"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(address) => this.setState({address})}
-                value={this.state.address}
-                placeholder = "Address"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(town) => this.setState({town})}
-                value={this.state.town}
-                placeholder = "Town"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 50, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(st8) => this.setState({st8})}
-                value={this.state.st8}
-                placeholder = "State"
-								placeholderTextColor = "black"
-                />
-
-                <TextInput
-                style={{height: 40, width: 100, margin: 10, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, width: "59%", borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
-                onChangeText={(gndr) => this.setState({gndr})}
-                value={this.state.gndr}
-                placeholder = "Gender"
-								placeholderTextColor = "black"
-                />
-								<View style={[{ width: "59%", margin: 10, backgroundColor: "purple" }]}>
-                <Button  onPress ={this.onPressEnterData.bind(this)}
-            			title="Submit"
-            			color='purple'
-            		/>
-								<Button  onPress ={this.onPressTest.bind(this)}
-            			title="Test"
-            			color='purple'
-            		/>
-								<Button  onPress ={this.getDate.bind(this)}
-            			title="Get Date"
-            			color='purple'
-            		/>
-								</View>
-
-
-								</View>
-                </ScrollView>
-                );
-    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -321,22 +179,13 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-		color:'black',
     margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: 'black',
+    color: '#333333',
     marginBottom: 5,
   },
-  license: {
-    height: 40,
-    width: 100,
-    borderColor: 'gray',
-    borderWidth: 2
- },
-
 });
-
 //needed since we are not using create-react-native-app
 AppRegistry.registerComponent('Test', () => UselessTextInput);
