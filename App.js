539cfcd946144cd5fe93798c3346ea9c221d5408
jerryxs <jerrygeffrard@yahoo.com>
2019-01-22 11:37:18 -0500

    Add files via upload
    Updated UI

--- App.js
+++ App.js
@@ -1,268 +1,314 @@
-import React, {
-    Component,
-    Document,
-} from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
+import React, {Component} from 'react';
 import {
-    AppRegistry,
-    Button,
-    TextInput,
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-} from 'react-native';
-
-import { RNCamera } from 'react-native-camera';
-
+	AppRegistry,
+	Button,
+	TextInput,
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+	} from 'react-native';
 import {
-    createStackNavigator,
-    StackActions,
-    NavigationActions
+	createStackNavigator,
+	stackActions,
+	NavigationActions,
 } from 'react-navigation';
+import nodejs from 'nodejs-mobile-react-native';
+import RNFetchBlob from 'rn-fetch-blob';
+import {sha256} from 'react-native-sha256';
+import { KeyboardAvoidingView } from 'react-native';
+
+
+//import { Zyre } from "zyre";
+
+//const Zyre = require('zyre.js');
+
+//New zyre object
+/*const zyre = new Zyre({
+  name: 'foo',      // Name of the zyre node
+  iface: 'eth0',    // Network interface
+  headers: {        // Headers will be sent on every new connection
+    foo: 'bar',
+  },
+  evasive: 5000,    // Timeout after which the local node will try to ping a not responding peer
+  expired: 30000,   // Timeout after which a not responding peer gets disconnected
+  port: 49152,      // Port for incoming messages, will be incremented if already in use
+  bport: 5670,      // Discovery beacon broadcast port
+  binterval: 1000,  // Discovery beacon broadcast interval
+});*/
+
+const instructions = Platform.select({
+
+  /*ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+	android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',*/
+
+});
+
+const dirs = RNFetchBlob.fs.dirs
+var path = dirs.DocumentDir + '/my.csv';
+	/*
+	 * Check to see if the CSV file exists
+	 * If it does then skip this portion as to not add the headers again
+	 * If not then create this file at the file path for android and add headers
+	 */
+	 RNFetchBlob.fs.exists(path)
+.then((exist) => {
+    if(exist === false){
+			RNFetchBlob.fs.writeStream(path, 'base64', true)
+		     .then((stream) => {
+		         stream.write(RNFetchBlob.base64.encode('licNum, DOB, fName, lName, address, town, state, gender ' + '\n'))
+		         return stream.close()
+		     })
+		}
+})
 
 type Props = {};
-class App extends Component<Props> {
-    
+export default class App extends Component<Props> {
+
+	//Add states for input boxes
     constructor(props) {
         super(props);
         this.state = {
-        licNum: "",
-        dob: "",
-        fName: "",
-        lName: "",
-        address: "",
-        town: "",
-        st8: "",
-        gndr: ""
+			licNum: "",
+			dob: "",
+			fName: "",
+			lName: "",
+			address: "",
+			town: "",
+			st8: "",
+			gndr: ""
         };
     }
 
+
+
+	//Function for submit button
     onPressEnterData(){
-        // Idea: when the submit button is pressed... then make the array
-        // for the csv + continue tutorial functions...
-        var csvData = [{
-                       // May not need to add the strings each time.
-                       // Probably makes more sense to append to the first row of the csv file the lables
-                       'LicenseNum' : this.state.licNum,
-                       'DateOfBirth' : this.state.dob,
-                       'FirstName' : this.state.fName,
-                       'LastName' : this.state.lName,
-                       'Address' : this.state.address,
-                       'Town' : this.state.town,
-                       'State' : this.state.st8,
-                       'Gender' : this.state.gndr
-                       }];
-        
-        // just makes a warning pop up with the data entered in the text boxes
-        console.warn(csvData);
+	  //When submit is pressed, an array is populated with the new state of each input box
+	  var csvData = [
+
+		 this.state.licNum,
+		 this.state.dob,
+		 this.state.fName,
+		 this.state.lName,
+		 this.state.address,
+		 this.state.town,
+		 this.state.st8,
+		 this.state.gndr
+
+	  ];
+	  sha256(csvData[0]).then( hash => {
+		  csvData[0] = hash
+		  console.warn(csvData[0]);
+		})
+		sha256(csvData[4]).then( hash => {
+		  csvData[4] = hash
+		  console.warn(csvData[4]);
+		})
+	  //console.warn("Directory: ", path);
+	  //Array is indexed properly can store csvData[0] into string to dup check
+	  //console.warn("Array[0]: ", csvData[0]);
+		  //Read file before adding new info to it
+		  //Currently just reads the file and spits out the info in a warn
+		  RNFetchBlob.fs.readStream(path, 'utf8')
+			.then((stream) => {
+				let data = ''
+				stream.open()
+				stream.onData((chunk) => {
+					data += chunk
+
+				})
+				stream.onEnd(() => {
+
+					if(data.includes(csvData[0]))
+					{
+						alert('Duplicate Warning!' + '\n' + 'Guest Already Entered The Event',)
+
+						//console.warn(csvData[0], " Is already in the file");
+					}
+					else
+					{
+
+						//Append the input data to the file
+						RNFetchBlob.fs.writeStream(path, 'base64', true)
+						.then((stream) => {
+							stream.write(RNFetchBlob.base64.encode(csvData + '\n'))
+							return stream.close()
+						})
+						alert('Guest Added To The Event List!')
+					}
+				})
+			})
+
+		  /*RNFetchBlob.fs.hash(path, 'sha256')
+		  .then((hash) => {
+
+
+			})*/
+
+
+
+
+
+
+	  // just makes a warning pop up with the data entered in the text boxes
+	  //console.warn(csvData);
+
     }
-    
+	//Backend for zyre
+	componentWillMount()
+  {
+    nodejs.start('main.js');
+    nodejs.channel.addListener(
+      'message',
+      (msg) => {
+        alert('From node: ' + msg);
+      },
+      this
+    );
+  }
+
+
+
     render() {
         return (
-                <View style={{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+					<KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+
+							  <View style={{flex: 1}}>
+                <View style={{flex: 2, backgroundColor: 'black',fontFamily:'Optima' }} />
                 <Text style={styles.welcome}>EManage</Text>
                 <Text style={styles.instructions}>Please Enter the Following Data</Text>
-                
+
+
+
+
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2 , textAlign: 'center' }}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2 , textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(licNum) => this.setState({licNum})}
                 value={this.state.licNum}
                 placeholder = "License Number"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(dob) => this.setState({dob})}
                 value={this.state.dob}
                 placeholder = "Date of Birth"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, width:1000, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(fName) => this.setState({fName})}
                 value={this.state.fName}
                 placeholder = "First Name"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(lName) => this.setState({lName})}
                 value={this.state.lName}
                 placeholder = "Last Name"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(address) => this.setState({address})}
                 value={this.state.address}
                 placeholder = "Address"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(town) => this.setState({town})}
                 value={this.state.town}
                 placeholder = "Town"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(st8) => this.setState({st8})}
                 value={this.state.st8}
                 placeholder = "State"
                 />
-                
+
                 <TextInput
-                style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 2}}
-                style={{height: 40, borderColor: 'gray', borderWidth: 2, textAlign: 'center'}}
+                style={{height: 40, width: 100, borderColor: 'black', borderWidth: 2}}
+                style={{height: 60, borderColor: 'black', borderWidth: 2, textAlign: 'center',fontSize: 20,fontWeight: 'bold'}}
                 onChangeText={(gndr) => this.setState({gndr})}
                 value={this.state.gndr}
                 placeholder = "Gender"
                 />
-                
-                <Button onPress ={this.onPressEnterData.bind(this)}
-                title="Submit"
-                color='purple'
-                />
-                
-                <Button
-                title="Open Camera"
-                onPress = {() => {
-                this.props.navigation.dispatch(StackActions.reset({
-                      index: 0,
-                      actions: [
-                                NavigationActions.navigate({ routeName: 'Camera'})
-                                ],
-                            }))
-                }}
-                color='steelblue'
-                />
-                
-                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+
+								<View style={{ backgroundColor: '#1D7AE7',borderRadius: 3,padding: 10}} >
+										<Button onPress ={this.onPressEnterData.bind(this)}
+            				title="Submit"
+            				color='black'
+										>
+            				</Button>
+									</View>
+
+			  	<View style={{ backgroundColor: '#D0071F',borderRadius: 3,padding: 10}} >
+						<Button onPress={() => nodejs.channel.send('A message!')}
+             title="Message Node"
+						 color="black"
+						>
+						</Button>
+						</View>
+                <View style={{flex: 2, backgroundColor: 'black'}} />
+
+
 
                 </View>
-                
+								</KeyboardAvoidingView>
+
                 );
     }
-    
 }
-
-class cameraScreen extends React.Component {
-    render() {
-        return(
-               
-               <View style={{flex: 2, backgroundColor: 'skyblue'}} >
-               
-               <RNCamera
-               ref={ref => {
-               this.camera = ref;
-               }}
-               style = {styles.preview}
-               type={RNCamera.Constants.Type.back}
-               flashMode={RNCamera.Constants.FlashMode.on}
-               permissionDialogTitle={'Permission to use camera'}
-               permissionDialogMessage={'We need your permission to use your camera phone'}
-               onGoogleVisionBarcodesDetected={({ barcodes }) => {
-               console.log(barcodes)
-               }}
-               />
-               
-               <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
-               <TouchableOpacity
-               onPress={this.takePicture.bind(this)}
-               style = {styles.capture}
-               >
-               <Text style={{fontSize: 14}}> SNAP </Text>
-               </TouchableOpacity>
-               </View>
-               
-               <Button
-               title="Return"
-               onPress = {() => {
-               this.props.navigation.dispatch(StackActions.reset({
-                     index: 0,
-                     actions: [
-                               NavigationActions.navigate({ routeName: 'UserInput'})
-                               ],
-                     }))
-               }}
-               color='steelblue'
-               />
-               
-               </View>
-               );
-    }
-    takePicture = async function(camera) {
-        try{
-        const options = { quality: 0.5, base64: true };
-        const data = await camera.takePictureAsync(options);
-        //  eslint-disable-next-line
-        console.log(data.uri);
-        } catch(e) {
-            console.log(e)
-        }
-    }
-}
-
-export default createStackNavigator({
-            Camera: {
-            screen: cameraScreen,
-            },
-            UserInput: {
-            screen: App,
-            },
-        }, { initialRouteName: 'UserInput',
-    });
 
 const styles = StyleSheet.create({
-                                 container: {
-                                 flex: 1,
-                                 justifyContent: 'center',
-                                 alignItems: 'center',
-                                 backgroundColor: '#F5FCFF',
-                                 },
-                                 welcome: {
-                                 fontSize: 20,
-                                 textAlign: 'center',
-                                 margin: 10,
-                                 },
-                                 instructions: {
-                                 textAlign: 'center',
-                                 color: '#333333',
-                                 marginBottom: 5,
-                                 },
-                                 license: {
-                                 height: 40,
-                                 width: 100,
-                                 borderColor: 'gray',
-                                 borderWidth: 2
-                                 },
-                                 preview: {
-                                 flex: 1,
-                                 justifyContent: 'flex-end',
-                                 alignItems: 'center',
-                                 },
-                                 capture: {
-                                 flex: 0,
-                                 backgroundColor: '#fff',
-                                 borderRadius: 5,
-                                 padding: 15,
-                                 paddingHorizontal: 20,
-                                 alignSelf: 'center',
-                                 margin: 20,
-                                 }
-                                 });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+		fontSize: 17
+  },
+  license: {
+    height: 40,
+    width: 100,
+    borderColor: 'black',
+    borderWidth: 2
+ },
+
+});
 
 //needed since we are not using create-react-native-app
 AppRegistry.registerComponent('Test', () => UselessTextInput);
