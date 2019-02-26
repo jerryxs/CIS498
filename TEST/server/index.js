2fbc53f84658adde3b5af89f87efef16511399ac
Ian <vladfan12@gmail.com>
2019-02-26 10:39:25 -0500

    Able to upload file to server
    Sending file to server works... will be in server/fileupload

--- TEST/server/index.js
+++ TEST/server/index.js
@@ -2,8 +2,35 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+var busboy = require("connect-busboy");
+var fs = require("fs");
+var path = require("path");
 
 server.listen(8000);
+
+app.use(busboy());
+//app.use(express.static(path.join(__dirname, "public")));
+
+/* ========================================================== 
+Create a Route (/upload) to handle the Form submission 
+(handle POST requests to /upload)
+Express v4  Route definition
+============================================================ */
+app.route("/upload").post(function(req, res, next) {
+  var fstream;
+  req.pipe(req.busboy);
+  req.busboy.on("file", function(fieldname, file, filename) {
+    console.log("Uploading: " + filename);
+
+    //Path where image will be uploaded
+    fstream = fs.createWriteStream(__dirname + "/fileupload/" + filename);
+    file.pipe(fstream);
+    fstream.on("close", function() {
+      console.log("Upload Finished of " + filename);
+      res.redirect("back"); //where to go next
+    });
+  });
+});
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
