775eea0617c2bf1404f586df23ee4df38a293270
jerryxs <jerrygeffrard@yahoo.com>
2019-03-16 18:22:19 -0400

    Banned List function on Submit buttons

--- App.js
+++ App.js
@@ -44,13 +44,108 @@ const storage = new Storage({
   sync: {}
 });
 
+const bannedListStorage = new Storage({
+  // maximum capacity, default 1000
+  size: 100000,
+
+  // Use AsyncStorage for RN apps, or window.localStorage for web apps.
+  // If storageBackend is not set, data will be lost after reload.
+  storageBackend: AsyncStorage, // for web: window.localStorage
+
+  // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
+  // can be null, which means never expire.
+  defaultExpires: 1000 * 3600 * 24,
+
+  // cache data in the memory. default is true.
+  enableCache: true,
+
+  // if data was not found in storage or expired data was found,
+  // the corresponding sync method will be invoked returning
+  // the latest data.
+  sync: {}
+});
+
 type Props = {};
 export default class App extends Component<Props> {
   //Add states for input boxes
   constructor(props) {
     super(props);
 
-    this.socket = io("http://172.18.15.245:8000"); // connects to the local server
+    this.state = {
+      licNum: "",
+      dob: "",
+      fName: "",
+      lName: "",
+      address: "",
+      town: "",
+      st8: "",
+      gndr: ""
+    };
+
+    this.socket = io("http://134.88.133.46:8000"); // connects to the local server
+    this.socket.on("noBannedList", () => {
+      console.warn("No Banned List detected!");
+    });
+    this.socket.on("gotBannedList", listData => {
+      var bannedList = listData.bannedList;
+      console.warn(bannedList.length);
+
+      bannedList.forEach(bannedGuest => {
+        console.warn(bannedGuest);
+        sha256(bannedGuest.licNum)
+          .then(hash => {
+            bannedGuest.licNum = hash;
+
+            return bannedGuest;
+          })
+          .then(bannedGuest => {
+            sha256(bannedGuest.address).then(hash => {
+              bannedGuest.address = hash;
+            });
+            return bannedGuest;
+          })
+          .then(bannedGuest => {
+            return bannedListStorage.save({
+              key: bannedGuest.licNum,
+              //id: bannedGuest.licNum,
+              data: bannedGuest,
+
+              expires: 1000 * 3600 * 24
+            });
+          });
+      });
+    });
+
+    this.socket.on("needGuestList", data => {
+      var guestList = data.guestList;
+
+      guestList.forEach(guest => {
+        var info = guest.dataStored;
+        console.warn(info);
+        sha256(info.licNum)
+          .then(hash => {
+            info.licNum = hash;
+
+            return info;
+          })
+          .then(info => {
+            sha256(info.address).then(hash => {
+              info.address = hash;
+            });
+            return info;
+          })
+          .then(info => {
+            return storage.save({
+              key: info.licNum,
+              data: info,
+
+              expires: 1000 * 3600
+            });
+          });
+      });
+      console.warn(storage);
+    });
+
     // Use this area to listen to signals from server and do something...
     this.socket.on("receiveUserData", data => {
       storage.save({
@@ -65,64 +160,128 @@ export default class App extends Component<Props> {
 
       alert("Guest Added To The Event List!");
     });
+  }
 
-    this.state = {
-      licNum: "",
-      dob: "",
-      fName: "",
-      lName: "",
-      address: "",
-      town: "",
-      st8: "",
-      gndr: ""
+  onPressTest2() {
+    var arrayObj = [];
+    var testData = {
+      licNum: "1000000",
+      dob: "12/04/1995",
+      fName: "Bhars",
+      lName: "Corcoran",
+      address: "123 Test st",
+      town: "Testville",
+      st8: "Testachussets",
+      gndr: "T"
     };
+
+    for (var x = 0; x < 50; x++) {
+      //testData.licNum = "S1000000";
+      var licNum = parseInt(testData.licNum);
+      licNum++;
+
+      //console.warn(licNum);
+
+      testData = {
+        licNum: licNum.toString(),
+        dob: "12/04/1995",
+        fName: "Bhars",
+        lName: "Corcoran",
+        address: "123 Test st",
+        town: "Testville",
+        st8: "Testachussets",
+        gndr: "T"
+      };
+
+      console.warn(testData);
+      sha256(testData.licNum)
+        .then(hash => {
+          testData.licNum = hash;
+          return testData;
+        })
+        .then(testData => {
+          sha256(testData.address).then(hash => {
+            testData.address = hash;
+          });
+          return testData;
+        })
+        .then(testData => {
+          arrayObj.push(testData);
+        });
+    }
+    console.warn(arrayObj);
   }
 
   onPressTest() {
-    //Need to set up new test... will only be size of max set above
+    const data = {
+      licNum: this.state.licNum,
+      dob: this.state.dob,
+      fName: this.state.fName,
+      lName: this.state.lName,
+      address: this.state.address,
+      town: this.state.town,
+      st8: this.state.st8,
+      gndr: this.state.gndr
+    };
 
-    var num = 1;
-    var temp = 10000000;
-    console.log("starting...");
-    var start = new Date();
+    sha256(data.licNum)
+      .then(hash => {
+        data.licNum = hash;
 
-    setInterval(() => {
-      for (num; num < 50; num++) {
-        var testData = {
-          licNum: temp,
-          dob: "10/25/1996",
-          fName: "Tester",
-          lName: "Smith",
-          address: "123 Street St",
-          town: "Testville",
-          st8: "MA",
-          gndr: "?"
-        };
-        sha256(testData.licNum)
-          .then(hash => {
-            testData.licNum = hash;
-            return testData;
+        return data;
+      })
+      .then(data => {
+        sha256(data.address).then(hash => {
+          data.address = hash;
+        });
+        return data;
+      })
+      .then(data => {
+        return bannedListStorage
+          .load({
+            key: data.licNum,
+
+            // autoSync (default: true) means if data is not found or has expired,
+            // then invoke the corresponding sync method
+            autoSync: true,
+
+            // syncInBackground (default: true) means if data expired,
+            // return the outdated data first while invoking the sync method.
+            // If syncInBackground is set to false, and there is expired data,
+            // it will wait for the new data and return only after the sync completed.
+            // (This, of course, is slower)
+            syncInBackground: true,
+
+            // you can pass extra params to the sync method
+            // see sync example below
+            syncParams: {
+              extraFetchOptions: {
+                // blahblah
+              },
+              someFlag: true
+            }
           })
-          .then(testData => {
-            sha256(testData.address).then(hash => {
-              testData.address = hash;
-            });
-            return testData;
+          .then(ret => {
+            // found data go to then()
+            alert("guest is banned");
           })
-          .then(testData => {
-            return storage.save({
-              // dynamic key
-              key: testData.licNum, // Note: Do not use underscore("_") in key!
-              data: testData,
-
-              // if expires not specified, the defaultExpires will be applied instead.
-              // if set to null, then it will never expire.
-              expires: 1000 * 3600
-            });
+          .catch(err => {
+            // any exception including data not found
+            // goes to catch()
+            console.warn(err.message);
+            switch (err.name) {
+              case "NotFoundError":
+                // TODO;
+                break;
+              case "ExpiredError":
+                // TODO
+                console.warn("here");
+                break;
+            }
           });
-        temp = temp + 1;
-      }
-    }, 1000);
+      });
+
+    console.warn(bannedListStorage);
   }
   //Function for submit button
   onPressEnterData() {
@@ -154,8 +313,14 @@ export default class App extends Component<Props> {
       .then(dataStored => {
         return storage.load({
           // same dynamic key
-          key: dataStored.licNum,
+            key: dataStored.licNum,
+        }
+        .then(data => {
+        return bannedListStorage
+          .load({
+            key: data.licNum,
 
+       
           // autoSync (default: true) means if data is not found or has expired,
           // then invoke the corresponding sync method
           autoSync: true,
@@ -176,6 +341,25 @@ export default class App extends Component<Props> {
           }
         });
       })
+         .then(ret => {
+            // found data go to then()
+            alert("guest is banned");
+          })
+          .catch(err => {
+            // any exception including data not found
+            // goes to catch()
+            console.warn(err.message);
+            switch (err.name) {
+              case "NotFoundError":
+                // TODO;
+                break;
+              case "ExpiredError":
+                // TODO
+                console.warn("here");
+                break;
+            }
+          }));
+        })
       .then(ret => {
         // found data go to then()
         showMessage({
@@ -227,6 +411,17 @@ export default class App extends Component<Props> {
               });
             }
 
+            this.setState({
+              licNum: "",
+              dob: "",
+              fName: "",
+              lName: "",
+              address: "",
+              town: "",
+              st8: "",
+              gndr: ""
+            });
+
             break;
           case "ExpiredError":
             // TODO
@@ -236,44 +431,34 @@ export default class App extends Component<Props> {
 
     // Resets the input boxes to empty after the submission
     // Taken out right now for test demonstration
-    /*	this.setState({licNum: "",
-										dob: "",
-										fName: "",
-										lName: "",
-										address: "",
-										town: "",
-										st8: "",
-										gndr: ""
-											})*/
+    /*this.setState({
+      licNum: "",
+      dob: "",
+      fName: "",
+      lName: "",
+      address: "",
+      town: "",
+      st8: "",
+      gndr: ""
+    });*/
+    console.warn(storage);
   }
 
   render() {
     return (
       <ScrollView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <View style={styles.container} />
+          <View style={{ flex: 1, backgroundColor: "skyblue" }} />
 
-          <View style={{ backgroundColor: "black",padding:10 }} />
+          <View style={{ flex: 1, backgroundColor: "steelblue" }} />
           <Text style={styles.welcome}>EManage</Text>
           <Text style={styles.instructions}>
             Please Enter the Following Data
           </Text>
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2,
-            }}
-            style={{
-              height: 55,
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontWeight: "bold",
-              fontSize: 25
-
-            }}
+            style={styles.textBox}
             onChangeText={licNum => this.setState({ licNum })}
             value={this.state.licNum}
             placeholder="License Number"
@@ -281,27 +466,11 @@ export default class App extends Component<Props> {
             onSubmitEditing={() => {
               this.dateInput.focus();
             }}
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold",
-
-            }}
+            style={styles.textBox}
             onChangeText={dob => this.setState({ dob })}
             value={this.state.dob}
             returnKeyType="next"
@@ -310,26 +479,11 @@ export default class App extends Component<Props> {
               this.firstNameInput.focus();
             }}
             placeholder="MM/DD/YYYY"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold",
-            }}
+            style={styles.textBox}
             onChangeText={fName => this.setState({ fName })}
             value={this.state.fName}
             returnKeyType="next"
@@ -338,26 +492,11 @@ export default class App extends Component<Props> {
               this.lastNameInput.focus();
             }}
             placeholder="First Name"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold",
-            }}
+            style={styles.textBox}
             onChangeText={lName => this.setState({ lName })}
             value={this.state.lName}
             returnKeyType="next"
@@ -366,26 +505,11 @@ export default class App extends Component<Props> {
               this.addressInput.focus();
             }}
             placeholder="Last Name"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold"
-            }}
+            style={styles.textBox}
             onChangeText={address => this.setState({ address })}
             value={this.state.address}
             returnKeyType="next"
@@ -394,26 +518,11 @@ export default class App extends Component<Props> {
               this.townInput.focus();
             }}
             placeholder="Address"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold"
-            }}
+            style={styles.textBox}
             onChangeText={town => this.setState({ town })}
             value={this.state.town}
             returnKeyType="next"
@@ -422,26 +531,11 @@ export default class App extends Component<Props> {
               this.st8Input.focus();
             }}
             placeholder="Town"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 50,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold"
-            }}
+            style={styles.textBox}
             onChangeText={st8 => this.setState({ st8 })}
             value={this.state.st8}
             returnKeyType="next"
@@ -450,48 +544,38 @@ export default class App extends Component<Props> {
               this.genderInput.focus();
             }}
             placeholder="State"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
 
           <TextInput
-            style={{
-              height: 40,
-              width: 100,
-              margin: 10,
-              borderColor: "gray",
-              borderWidth: 2
-            }}
-            style={{
-              height: 55,
-              width: "100%",
-              borderColor: "gray",
-              borderWidth: 2,
-              textAlign: "center",
-              fontSize: 25,
-              fontWeight: "bold"
-            }}
+            style={styles.textBox}
             onChangeText={gndr => this.setState({ gndr })}
             value={this.state.gndr}
             returnKeyType="go"
             ref={input => (this.genderInput = input)}
             placeholder="Gender"
-            placeholderTextColor="black"
+            placeholderTextColor="gray"
           />
           <View
-            style={[{ flex:1,width: "100%", margin: 10, backgroundColor: "#1D7AE7",alighItem: "center" }]}
+            style={[{ width: "59%", margin: 10, backgroundColor: "purple" }]}
           >
             <Button
               onPress={this.onPressEnterData.bind(this)}
               title="Submit"
-              color="#1D7AE7"
+              color="purple"
             />
-            </View>
             <Button
               onPress={this.onPressTest.bind(this)}
               title="Test"
-              color="red"
+              color="purple"
             />
-
+            <Button
+              onPress={this.onPressTest2.bind(this)}
+              title="Test2"
+              color="purple"
+            />
+          </View>
+        </View>
         <FlashMessage position="top" />
       </ScrollView>
     );
@@ -501,20 +585,27 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 10,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
   welcome: {
-    fontSize: 35,
+    fontSize: 20,
+    color: "black",
     textAlign: "center",
     margin: 10
   },
   instructions: {
-    fontSize: 20,
     textAlign: "center",
-    color: "black",
+    color: "#333333",
     marginBottom: 5
-    
+  },
+  textBox: {
+    height: 40,
+    width: "59%",
+    borderColor: "gray",
+    borderWidth: 2,
+    textAlign: "center"
   }
 });
 //needed since we are not using create-react-native-app
